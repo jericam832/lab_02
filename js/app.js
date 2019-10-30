3c91b33231b735f0c5ecb3b1fad34339035eb42a
jericam832 <jericam32@gmail.com>
2019-10-30 16:14:05 -0700

    added comments to appjs

--- js/app.js
+++ js/app.js
@@ -1,27 +1,25 @@
 'use strict';
 
-
 $(document).ready(function() {
+    let seen = {};
     $.get('data/page-1.json')
     .then( hornImg => {
         hornImg.forEach( data => { 
-            let createImg = $("<img></img>");
-            createImg.attr('src', `${data.image_url}`);
-            createImg.attr('value', `${data.keyword}`);
-            $('#photo-template').append(createImg);
-            let createOpt = $("<option></option>");
-            createOpt.attr('value', `${data.keyword}`);
-            createOpt.text(`${data.keyword}`);
-            $('select').append(createOpt);         
+            let createImg = $("<img></img>");//create an image tag
+            createImg.attr('src', `${data.image_url}`);//assign a source to the image tag
+            createImg.attr('value', `${data.keyword}`);//assign a value attribute to the image tag
+            $('#photo-template').append(createImg);//append image to the section tag
+                let createOpt = $("<option></option>");//create an option tag
+                createOpt.attr('value', `${data.keyword}`);//assign a keyword to the option tag
+                createOpt.text(`${data.keyword}`);//assign a value attribute to the option tag
+                $('select').append(createOpt); //append the option tag to the dropdown      
         });
-        console.log(hornImg);
-        console.log(createOpt);
     })
 });
 
 $('select').on('change', function() {
     let selectedValue = $(this).val();
     $('img').hide();
-    $(`img[value=${selectedValue}]`).show();
+    $(`img[value=${selectedValue}]`).fadeIn(2000);
     console.log(selectedValue);
 });
